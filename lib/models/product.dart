8bf0fcdf51df3efa20b35f65189ec3beb415e014
boran <boranpekell@hotmail.com>
2019-07-17 14:46:29 +0300

    add getters and setters

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -6,4 +6,23 @@ class Product{
 
   Product(this.name,this.description,this.price);
   Product.withId(this.id,this.name,this.description,this.price);
+
+  int get getId => id;
+  String get getName => name;
+  String get getDescription => description;
+  double get getPrice => price;
+
+  set setName(String name){
+    if(name.length > 2)
+      this.name = name;
+  }
+
+  set setPrice(double price){
+    if(price > 0)
+      this.price = price;
+  }
+
+  set setDescription(String description){
+    this.description = description;
+  }
 }
